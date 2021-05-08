b49d7b5bb95fdfb6a1f3558ed1aee0043a804999
Ramakrishna Natarajan <ramakrishna.natarajan@mail.utoronto.ca>
2021-05-09 02:28:51 +0800

    event loop to print markers

--- gaia/events/views.py
+++ gaia/events/views.py
@@ -7,3 +7,9 @@ class EventView(CreateView):
     fields = ['event']
     template_name = 'events/home.html'
     success_url = '/'
+
+    def get_context_data(self, **kwargs):
+       context =  super().get_context_data(**kwargs)
+       context['mapbox_access_token'] = 'pk.eyJ1IjoibmtyYW1hOTkiLCJhIjoiY2tvZncwbjE1MGF0dTJvcG5uM3dlZTVqaCJ9.5hAFk9giRgnqm8SmSfFz3Q'
+       context['events'] = Event.objects.all()
+       return context
