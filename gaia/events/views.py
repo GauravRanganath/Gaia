48107024568f3a9c09cb6b841cc1ddadb97908f3
Gaurav Ranganath <gaurav.ranganath@mail.utoronto.ca>
2021-05-08 18:47:05 -0400

    forgot to push views

--- gaia/events/views.py
+++ gaia/events/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView
 from .models import Event
 
@@ -7,6 +8,18 @@ class EventView(CreateView):
     fields = ['location']
     template_name = 'events/home.html'
     success_url = '/'
+
+    def get_context_data(self, **kwargs):
+       context =  super().get_context_data(**kwargs)
+       context['mapbox_access_token'] = 'pk.eyJ1IjoibmtyYW1hOTkiLCJhIjoiY2tvZncwbjE1MGF0dTJvcG5uM3dlZTVqaCJ9.5hAFk9giRgnqm8SmSfFz3Q'
+       context['events'] = Event.objects.all()
+       return context
+
+class CreateEventView(CreateView):
+    model = Event
+    fields = ['location', 'event_name']
+    template_name = 'events/create_event.html'
+    success_url = 'create_event'
 
     def get_context_data(self, **kwargs):
        context =  super().get_context_data(**kwargs)
