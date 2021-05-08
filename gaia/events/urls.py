78ac0bccba3e806481e537c88f24b0efadbcb7bc
Gaurav Ranganath <gaurav.ranganath@mail.utoronto.ca>
2021-05-08 17:06:23 -0400

    added new page form

--- gaia/events/urls.py
+++ gaia/events/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import EventView
+from .views import CreateEventView
+from . import views
+from django.shortcuts import render
 
 urlpatterns = [
     path('', EventView.as_view(), name='home'),
-]
+    path('create_event/', CreateEventView.as_view(), name='create_event'),
+]   
