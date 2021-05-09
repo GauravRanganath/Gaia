e6e0c12244b65bb0e3663c329e775e335bc00676
Ramakrishna Natarajan <ramakrishna.natarajan@mail.utoronto.ca>
2021-05-09 10:00:58 +0800

    userprofile model is visible on admin

--- gaia/userProfiles/models.py
+++ gaia/userProfiles/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class UserProfile(models.Model):
     name = models.CharField(max_length=255, default="<noname>")
     email = models.CharField(max_length=255, default="<noemail>")
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=12)
     location = models.TextField()
